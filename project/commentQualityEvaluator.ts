9c5c8ab52d5d223507e4283a1ffb6dc8e7a8ba19
Jan Philipp Sachse <jan.sachse@student.hpi.de>
2017-08-21 13:12:28 +0200

    Classify pure code comments as unhelpful

--- project/commentQualityEvaluator.ts
+++ project/commentQualityEvaluator.ts
@@ -1,4 +1,27 @@
+import { CommentClass, ICommentClassification, ICommentPart, SourceComment } from "./sourceComment";
+import { SourceMap } from "./sourceMap";
+
+export enum CommentQuality {
+    Unknown,
+    Unhelpful,
+    Low,
+    Medium,
+    High,
+}
 
 export default class CommentQualityEvaluator {
+
+    public evaluateQuality(comment: SourceComment, sourceMap: SourceMap): CommentQuality {
+        const pureCodeComment = (classification: ICommentClassification): boolean => {
+            return classification.lines === undefined &&
+                    classification.commentClass === CommentClass.Code;
+        };
+        if (comment.classifications.find(pureCodeComment)) {
+            return CommentQuality.Unhelpful;
+        }
+        const commentEndLine = sourceMap.sourceFile.getLineAndCharacterOfPosition(comment.end).line;
+        const nextNode = sourceMap.getNodeAfterLine(commentEndLine);
+        return CommentQuality.Unknown;
+    }
 
 }
