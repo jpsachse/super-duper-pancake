0c819de84ed4546d5bfdd7e82d969e33ee978d60
Jan Philipp Sachse <jan.sachse@student.hpi.de>
2017-08-21 15:35:08 +0200

    Add unhandled SyntaxKinds to isDeclaration

--- project/utils.ts
+++ project/utils.ts
@@ -34,19 +34,29 @@ export default class Utils {
     }
 
     public static isDeclaration(node: ts.Node): node is ts.Declaration {
-        return (node.kind === ts.SyntaxKind.PropertyDeclaration ||
-                node.kind === ts.SyntaxKind.MethodDeclaration ||
-                node.kind === ts.SyntaxKind.VariableDeclaration ||
-                node.kind === ts.SyntaxKind.FunctionDeclaration ||
-                node.kind === ts.SyntaxKind.ClassDeclaration ||
-                node.kind === ts.SyntaxKind.InterfaceDeclaration ||
-                node.kind === ts.SyntaxKind.TypeAliasDeclaration ||
+        // Possibly also handle these:
+        // ts.SyntaxKind.CallSignatureDeclaration
+        // ts.SyntaxKind.ConstructSignatureDeclaration
+        // ts.SyntaxKind.Parameter
+        // ts.SyntaxKind.IndexSignature
+        // special case: ts.SyntaxKind.VariableDeclarationList, which has an array of VariableDeclarations
+        return (node.kind === ts.SyntaxKind.ClassDeclaration ||
+                node.kind === ts.SyntaxKind.Constructor ||
                 node.kind === ts.SyntaxKind.EnumDeclaration ||
+                node.kind === ts.SyntaxKind.ExportDeclaration ||
+                node.kind === ts.SyntaxKind.FunctionDeclaration ||
+                node.kind === ts.SyntaxKind.GetAccessor ||
+                node.kind === ts.SyntaxKind.ImportDeclaration ||
+                node.kind === ts.SyntaxKind.ImportEqualsDeclaration ||
+                node.kind === ts.SyntaxKind.InterfaceDeclaration ||
+                node.kind === ts.SyntaxKind.MethodDeclaration ||
                 node.kind === ts.SyntaxKind.ModuleDeclaration ||
                 node.kind === ts.SyntaxKind.NamespaceExportDeclaration ||
-                node.kind === ts.SyntaxKind.ImportEqualsDeclaration ||
-                node.kind === ts.SyntaxKind.ImportDeclaration ||
-                node.kind === ts.SyntaxKind.ExportDeclaration);
+                node.kind === ts.SyntaxKind.PropertyDeclaration ||
+                node.kind === ts.SyntaxKind.SetAccessor ||
+                node.kind === ts.SyntaxKind.TypeAliasDeclaration ||
+                node.kind === ts.SyntaxKind.TypeParameter ||
+                node.kind === ts.SyntaxKind.VariableDeclaration);
     }
 
 }
