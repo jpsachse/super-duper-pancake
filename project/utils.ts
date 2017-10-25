4d3a9447de76ecea947cf2d86d85cac27c62b6c7
Jan Philipp Sachse <jan.sachse@student.hpi.de>
2017-10-25 18:24:00 +0200

    Fix using Windows newlines on darwin platforms 🙄

--- project/utils.ts
+++ project/utils.ts
@@ -7,7 +7,7 @@ export default class Utils {
 
     public static commentOnlyLineRegxp = /^\s*(\/\/|\/\*)/gm;
 
-    public static newLineChar = process.platform.includes("win") ? "\r\n" : "\n";
+    public static newLineChar = process.platform.startsWith("win") ? "\r\n" : "\n";
 
     public static createRange(start: number, end: number) {
         return Array.from({length: end - start + 1}, (value, key) => key + start);
