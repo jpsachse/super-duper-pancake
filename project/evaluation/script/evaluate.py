35e47d85b52b9622cc004aff14d27a2281be2bcc
Jan Philipp Sachse <jan.sachse@student.hpi.de>
2017-11-27 11:48:23 +0100

    Refator evaluation script

--- project/evaluation/script/evaluate.py
+++ project/evaluation/script/evaluate.py
@@ -17,14 +17,79 @@ class QuestionColumns:
     marked1, marked1a, marked2, marked2a, marked3, marked3a, marked4, marked4a, marked5, marked5a, marked6,\
         marked6a, marked7, marked7a, marked8, marked8a, marked9, marked9a, marked10, marked10a = range(17, 37)
 
-class ChartLabels:
-    xAxisFreeQuestions = "Comment Requirement Location Line Numbers"
-    xAxisPreFilledQuestions = "Highlighted Comment Requirement Locations"
+
+def no_typescript_no_programming(row):
+    return (row[QuestionColumns.experienceTypeScript] == QuestionAnswers.noTypeScript or
+            row[QuestionColumns.experienceProgramming] == QuestionAnswers.noProgramming)
+
+
+def load_answers(filename, only_typescript_developers):
+    result = {}
+    with open(filename) as opened_file:
+        reader = csv.reader(opened_file, delimiter=';', quotechar='"')
+        questions = {k: v for k, v in vars(QuestionColumns).iteritems() if not k.startswith("__")}
+
+        # get all answers as list, accessible by question identifier
+        next(reader)  # skip the header
+        for row in reader:
+            if only_typescript_developers and no_typescript_no_programming(row):
+                continue
+            for question_id, question_column in questions.iteritems():
+                question_answers = result.get(question_id, [])
+                current_answer = re.sub(r"\s+", "", row[question_column]).split(",")
+                question_answers += current_answer
+                result[question_id] = question_answers
+
+        # group answers by line
+        for question, answer_list in result.iteritems():
+            sorted_answers = sorted([a for a in answer_list])
+            # grouped_answers = [[answer, len(list(group))] for answer, group in groupby(sorted_answers)]
+            # grouped_answers.sort(lambda a, b: b[1] - a[1])
+            grouped_answers = {answer: len(list(group)) for answer, group in groupby(sorted_answers)}
+            result[question] = grouped_answers
+    return result
+
+
+def generate_charts(question_names, question_line_match_counts, template_filename):
+    class ChartLabels:
+        xAxisFreeQuestions = "Line Numbers of Locations Requiring Additional Comment"
+        xAxisPreFilledQuestions = "Highlighted Locations Requiring Additional Comment"
+
+    result = []
+    template = ""
+    with open(chart_template_filename) as chart_template_file:
+        template = chart_template_file.read()
+
+    for question_name in sorted(question_names.iterkeys(), key=natural_keys):
+        question_identifier = question_names[question_name]
+        line_match_counts = question_line_match_counts[question_identifier]
+        current_chart = template
+        survey_image_path = "survey_images/"
+        if question_identifier.startswith("q"):
+            survey_image_path += "01_unmarked/"
+            current_chart = current_chart.replace("PLACEHOLDER_X_LABEL", ChartLabels.xAxisFreeQuestions)
+        else:
+            survey_image_path += "02_marked/"
+            current_chart = current_chart.replace("PLACEHOLDER_X_LABEL", ChartLabels.xAxisPreFilledQuestions)
+        survey_image_path += question_identifier + ".png"
+        current_chart = current_chart.replace("PLACEHOLDER_SURVEY_IMAGE", survey_image_path)
+        current_chart = current_chart.replace("PLACEHOLDER_CAPTION", question_name)
+        current_chart = current_chart.replace("PLACEHOLDER_LABEL", "fig:" + question_identifier)
+        x_keys = ",".join(sorted(line_match_counts.keys(), key=natural_keys))
+        current_chart = current_chart.replace("PLACEHOLDER_X_COORDS", x_keys)
+
+        values = []
+        for line in sorted(line_match_counts.keys(), key=natural_keys):
+            count = line_match_counts[line]
+            values.append("(" + str(line) + "," + str(count) + ")")
+        current_chart = current_chart.replace("PLACEHOLDER_VALUES", ("\n" + " " * 16).join(values))
+        result.append(current_chart)
+    return result
+
 
 # mapping from question name to identifier, e.g., "Question 11" => "marked1"
 QUESTION_NAMES = {"Question " + str(x): "q" + str(x) for x in range(1, 11)}
 QUESTION_NAMES.update({"Question " + str(x + 10): "marked" + str(x) for x in range(1, 11)})
-TS_DEVS_ONLY = False
 
 filenames = []
 with open("filenames.txt") as filenames_file:
@@ -32,30 +97,7 @@ with open("filenames.txt") as filenames_file:
 
 csv_filename, prediction_filename, chart_template_filename, chart_output_filename = filenames
 
-answers = {}
-with open(csv_filename) as opened_file:
-    reader = csv.reader(opened_file, delimiter=';', quotechar='"')
-    questions = {k: v for k, v in vars(QuestionColumns).iteritems() if not k.startswith("__")}
-
-    # get all answers as list, accessible by question
-    next(reader)  # skip the header
-    for row in reader:
-        if TS_DEVS_ONLY and (row[QuestionColumns.experienceTypeScript] == QuestionAnswers.noTypeScript or
-                row[QuestionColumns.experienceProgramming] == QuestionAnswers.noProgramming):
-            continue
-        for question, question_column in questions.iteritems():
-            question_answers = answers.get(question, [])
-            current_answer = re.sub(r"\s+", "", row[question_column]).split(",")
-            question_answers += current_answer
-            answers[question] = question_answers
-
-    # group answers by line
-    for question, answer_list in answers.iteritems():
-        sorted_answers = sorted([a for a in answer_list])
-        # grouped_answers = [[answer, len(list(group))] for answer, group in groupby(sorted_answers)]
-        # grouped_answers.sort(lambda a, b: b[1] - a[1])
-        grouped_answers = { answer: len(list(group)) for answer, group in groupby(sorted_answers) }
-        answers[question] = grouped_answers
+answers = load_answers(csv_filename, False)
 
 matched_predictions = {}
 predictions = json.load(open(prediction_filename))
@@ -66,35 +108,6 @@ for question, predicted_lines in predictions.iteritems():
         matched_lines[predicted_line] = answer_lines.get(predicted_line, 0)
     matched_predictions[question] = matched_lines
 
-chart_template = ""
-with open(chart_template_filename) as chart_template_file:
-    chart_template = chart_template_file.read()
-
-all_charts = []
-for question_name in sorted(QUESTION_NAMES.iterkeys(), key=natural_keys):
-    question_identifier = QUESTION_NAMES[question_name]
-    matched_prediction = matched_predictions[question_identifier]
-    current_chart = chart_template
-    survey_image_path = "survey_images/"
-    if question_identifier.startswith("q"):
-        survey_image_path += "01_unmarked/"
-        current_chart = current_chart.replace("PLACEHOLDER_X_LABEL", ChartLabels.xAxisFreeQuestions)
-    else:
-        survey_image_path += "02_marked/"
-        current_chart = current_chart.replace("PLACEHOLDER_X_LABEL", ChartLabels.xAxisPreFilledQuestions)
-    survey_image_path += question_identifier + ".png"
-    current_chart = current_chart.replace("PLACEHOLDER_SURVEY_IMAGE", survey_image_path)
-    current_chart = current_chart.replace("PLACEHOLDER_CAPTION", question_name)
-    current_chart = current_chart.replace("PLACEHOLDER_LABEL", "fig:" + question)
-    x_keys = ",".join(sorted(matched_prediction.keys(), key=natural_keys))
-    current_chart = current_chart.replace("PLACEHOLDER_X_COORDS", x_keys)
-
-    values = []
-    for line in sorted(matched_prediction.keys(), key=natural_keys):
-        count = matched_prediction[line]
-        values.append("(" + str(line) + "," + str(count) + ")")
-    current_chart = current_chart.replace("PLACEHOLDER_VALUES", ("\n" + " " * 16).join(values))
-    all_charts.append(current_chart)
-
+all_charts = generate_charts(QUESTION_NAMES, matched_predictions, chart_template_filename)
 with open(chart_output_filename, "w") as chart_file:
     chart_file.write("\n\n".join(all_charts))
